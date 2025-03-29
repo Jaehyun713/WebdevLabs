18987da9c749682452d2f4488542f13e979c4a1f
Jaehyun713 <115044152+Jaehyun713@users.noreply.github.com>
2025-03-29 00:06:39 -0400

    Add files via upload

---
+++ webdevproject/static/script.js
@@ -0,0 +1,79 @@
+// Image lightbox
+// Open the Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
+// Image lightbox
+
+function validate() {
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const validationMessage = document.getElementById("validationMessage")
+    if ((!name.checkValidity()) || (!email.checkValidity()) || (!comment.checkValidity)){
+        validationMessage.innerHTML = "Please fill this field";
+    }
+}
+
+function zipValidate(){
+    const zipCode = document.getElementById("zip")
+    const validationMessage = document.getElementById("validationMessage")
+    if (!zipCode.checkValidity()){
+        validationMessage.innerHTML = "Invalid. Must be 5 digits"
+    }
+}
+
+function phoneValidate(){
+    const phoneNumber = document.getElementById("Phone Number")
+    const validationMessage = document.getElementById("validationMessage")
+    if (!phoneNumber.checkValidity()){
+        validationMessage.innerHTML = "Invalid. Must be 5 digits"
+    }
+} 
+
+$("#readMore").click(function(){
+    $("#history").show();
+    $("#readLess").show();
+    $("#readMore").hide();
+});
+
+$("#readLess").click(function(){
+    $("#history").hide();
+    $("#readLess").hide();
+    $("#readMore").show();
+});
